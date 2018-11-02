37018c8761d9672716989c46e2bf732a6b5fc89b
3216005218 <1181862531@qq.com>
2018-11-02 11:13:55 +0800

    完成搜索样式

--- src/components/index/components/search.vue
+++ src/components/index/components/search.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="searchBigBox">
     <div class="searchBox">
-      <div class="back">
-        <img :src="backSrc" alt="back">
-      </div>
+      <router-link to="/index">
+        <div class="back">
+          <img :src="backSrc" alt="back">
+        </div>
+      </router-link>
       <div>
         <input type="text" placeholder="完成PPT" class="searchInput">
       </div>
@@ -21,78 +23,172 @@
           <img :src="deleteSrc" alt="delete">
         </div>
       </div>
+      <div class="historyContentBox">
+        <div v-for="(item,index) in historyList" :key="index">
+          <span>{{item}}</span>
+        </div>
+      </div>
     </div>
-    <div class="hotBox"></div>
+    <div class="hotBox">
+      <div class="hotHead">
+        <img :src="hotSrc" alt="hot">
+        <p>热搜</p>
+      </div>
+      <div class="hotContentBox">
+        <div v-for="(item,index) in historyList" :key="index">
+          <span>{{item}}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-   export default {
-      name: 'search',
-      data(){
-        return{
-          backSrc: require('@/assets/searchBack.png'),
-          searchSrc: require('@/assets/search2.png'),
-          historySrc: require('@/assets/searchHistory.png'),
-          hotSrc: require('@/assets/searchHot.png'),
-          deleteSrc: require('@/assets/searchDelete.png')
+  export default {
+    name: 'search',
+    data() {
+      return {
+        backSrc: require('@/assets/searchBack.png'),
+        searchSrc: require('@/assets/search2.png'),
+        historySrc: require('@/assets/searchHistory.png'),
+        hotSrc: require('@/assets/searchHot.png'),
+        deleteSrc: require('@/assets/searchDelete.png'),
+        historyList: [
+          '奶茶',
+          'ppt制作',
+          '香飘飘',
+          '奶茶',
+          'ppt制作',
+          '香飘飘',
+          '奶茶',
+          '香飘飘aaaaaa'
+        ]
 
-        }
       }
-   }
+    }
+  }
+
 </script>
 
 <style scoped>
   .el-main {
     padding: 0;
   }
-  #searchBigBox{
-    background: rgb(255,255,255);
+
+  #searchBigBox {
+    background: rgb(255, 255, 255);
     width: 100%;
     height: 100%;
   }
-  .searchBox{
+
+  .searchBox {
     width: 100%;
     height: 50px;
     box-sizing: border-box;
-    background-color: #f9f9f9; 
+    background-color: #f9f9f9;
     padding: 0 10px;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    margin-bottom: 33px;
   }
-  .searchInput{
+
+  .searchInput {
     box-sizing: border-box;
     width: 272px;
     height: 35px;
     border-radius: 5px;
-    padding:2px 10px;  
+    padding: 2px 10px;
     font-size: 16px;
   }
-  .searchBox img{
-    width:28px;
+
+  .searchBox img {
+    width: 28px;
     height: 28px;
   }
-  .historyBox{
-   margin:28px 15px 0px 20px;
+
+  .historyBox {
+    box-sizing: border-box;
+    padding: 0px 15px 0px 20px;
+    width: 100%;
+    height: 80px;
+    margin-bottom: 45px;
   }
-  .historyBox img{
+
+  .hotBox img,
+  .historyBox img {
     width: 14px;
     height: 14px;
-   
+
   }
-  .historyHead{
+
+  .historyHead {
     display: flex;
     justify-content: space-between
   }
-  .historyBox img[alt="history"]{
+
+  .historyBox img[alt="history"],
+  img[alt="hot"] {
     margin-right: 5px;
   }
-   .historyIco{
-     display: flex
-   }
-  .historyIco p{
+
+  .hotHead,
+  .historyIco {
+    display: flex
+  }
+
+  .hotHead p,
+  .historyIco p {
     font-size: 14px;
     flex: 1;
   }
-  
+
+  .hotContentBox,
+  .historyContentBox {
+    width: auto;
+    height: auto;
+    padding: 10px 8px 10px 0px;
+    display: flex;
+    flex-wrap: wrap;
+
+  }
+
+  .historyContentBox div {
+    height: 10px;
+    width: auto;
+    display: inline-block;
+    padding: 6px 10px;
+    border: 1px solid rgb(239, 239, 239);
+    margin: 5px 7px 5px 0;
+  }
+
+  .historyContentBox div span {
+    font-size: 14px;
+    line-height: 10px;
+    color: #666666;
+  }
+
+  .hotBox {
+    box-sizing: border-box;
+    padding: 0px 15px 0px 20px;
+    width: 100%;
+    height: 80px;
+  }
+
+  .hotContentBox div {
+    height: 15px;
+    width: auto;
+    display: inline-block;
+    padding: 6px 10px;
+    margin: 5px 7px 5px 0;
+    border-radius: 10px;
+    background: rgb(247, 246, 246);
+    color: #666666;
+  }
+
+  .hotContentBox div span {
+    font-size: 14px;
+    line-height: 15px;
+    color: #666666;
+  }
+
 </style>
